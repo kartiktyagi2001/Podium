90f8d64495f82cbba1d4646e8f801b7ffa79c823
Kartik Tyagi <23mca063@jssaten.ac.in>
2025-08-12 04:44:04 +0530

    fixed vercel deployement-time bug

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+// import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../components/navbar";
 import ParticleBackground from "../components/particle-bg";
