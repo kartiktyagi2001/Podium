22859044191c3db49a651576814c63d56ffd8dee
rockyroed <roed-imp.vo97b@slmail.me>
2025-08-22 10:28:06 +0800

    fix background height to cover entire screen

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -26,7 +26,7 @@ export const Blog = ()=>{
 
     return (
 
-        <div className="bg-gradient-to-r h-full md:h-screen w-screen from-purple-100 via-slate-100 to-pink-100">
+        <div className="bg-gradient-to-r min-h-screen w-screen from-purple-100 via-slate-100 to-pink-100">
             <SingleBlog blog={post} />
         </div>
     )
